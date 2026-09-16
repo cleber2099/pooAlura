2a46c1f7362c58977a3079e7afa07f769a2e4749
cleber <cleber.santos.2099@gmail.com>
2026-09-16 13:29:02 -0300

    atributos privados

--- screenmatch/src/App.java
+++ screenmatch/src/App.java
@@ -1,14 +1,19 @@
 package screenmatch.src;
 public class App {
     public static void main(String[] args) throws Exception {
- Filme meuFilme = new Filme();
- meuFilme.nome = "O poderoso chefão";
- meuFilme.anoDeLancamento = 1970;
- meuFilme.duracaoEmMinutos = 180;
- 
- System.out.println("Nome do filme: " + meuFilme.nome);
- System.out.println("Ano de lançamento: " + meuFilme.anoDeLancamento);
- System.out.println("Duração em minutos: " + meuFilme.duracaoEmMinutos);
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+            System.out.println("Média das avaliações: " + meuFilme.pegaMedia());
+            System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+
+        
     }
 
 }
--- screenmatch/src/Filme.java
+++ screenmatch/src/Filme.java
@@ -3,9 +3,29 @@ package screenmatch.src;
 public class Filme {
     String nome;
     boolean incluidoNoPlano;
-    double avaliacao;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
     int anoDeLancamento;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 
 }
